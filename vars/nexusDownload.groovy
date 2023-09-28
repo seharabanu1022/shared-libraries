6d475eeb7957430b6e4aff6fb716199c1641f83c
seharabanu1022 <144198495+seharabanu1022@users.noreply.github.com>
2023-09-28 15:25:12 +0530

    Update nexusDownload.groovy

--- vars/nexusDownload.groovy
+++ vars/nexusDownload.groovy
@@ -3,7 +3,6 @@ def NEXUS_URUL = "http://172.31.30.87:8081"
 groupId = groupId.replace(".","/")
 echo groupId
 sh """
-curl -u admin:admin -o ${outputFile} -X GET
-"${NEXUS_URUL}/repository/${repoName}/${groupId}/${artifactId}/${version}/doctor-online-${version}.${extension}"
+curl -u admin:admin -o ${outputFile} -X GET"${NEXUS_URUL}/repository/${repoName}/${groupId}/${artifactId}/${version}/doctor-online-${version}.${extension}"
 """
 }
